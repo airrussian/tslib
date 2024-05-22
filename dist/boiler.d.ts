ea6c961fcd7743f131a48623cfe28cac3857c24b
Алексей <a.voronenko@zota.ru>
2024-05-22 19:32:20 +0700

    a

---
+++ dist/boiler.d.ts
@@ -0,0 +1,7 @@
+export declare class Boiler {
+    protected type: number;
+    protected serial: number;
+    protected password: number;
+    constructor(type: number, serial: number, password: number);
+    getId(): string;
+}
---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import { Boiler as _Boiler } from './boiler';
+export declare namespace Zota {
+    type Boiler = _Boiler;
+    const Boiler: typeof _Boiler;
+}
